ac2975d8c4eb01653a176188b8646941d65a9d1c
John William <johnwill.v.2017@gmail.com>
2021-11-17 13:14:34 -0300

    add struct na biblioteca

--- src/arvoreB.c
+++ src/arvoreB.c
@@ -1,7 +1,21 @@
+/*
+ * Autores: 
+ *  - John William Vicente <ra118237@uem.br>
+ *  - Gabriel Valentim de Oliveira Dacie <ra118419@uem.br>
+*/
+
 #include "arvoreB.h"
 
 #include <stdio.h>
 
-void ola() {
-    printf("OI");
+void geraArvoreB(char* nomeArquivo){
+
+}
+
+void imprimeArvoreB(){
+
+}
+
+void mostraChavesOrdenadasArvoreB(){
+
 }
--- src/arvoreB.h
+++ src/arvoreB.h
@@ -1,9 +1,23 @@
+/*
+ * Autores: 
+ *  - John William Vicente <ra118237@uem.br>
+ *  - Gabriel Valentim de Oliveira Dacie <ra118419@uem.br>
+*/
+
 #ifndef _ARVORE_B_
 #define _ARVORE_B_
 
 #define ARQUIVO_DADOS "btree.dat"
 #define ORDEM_ARVORE_B 4
 
-void geraArvoreB();
+typedef struct {
+    int numeroDeChaves;           /* número de chaves na página */
+    char chaves[ORDEM_ARVORE_B-1]; /* vetor que armazena as chaves */
+    int filhos[ORDEM_ARVORE_B];    /* RRNs dos filhos */
+} Pagina;
+
+void geraArvoreB(char*);
+void imprimeArvoreB();
+void mostraChavesOrdenadasArvoreB();
 
 #endif
